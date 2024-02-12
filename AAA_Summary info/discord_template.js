12c173f98129a50725b152aa82b8f5eed0e0df76
Ian Lewis <ijl20@cam.ac.uk>
2024-02-12 23:24:06 +0000

    discord template WIP

--- AAA_Summary info/discord_template.js
+++ AAA_Summary info/discord_template.js
@@ -10,7 +10,7 @@ SSC Task Title
 
 **MSFS Server:** Southeast Asia
 
-**Sim date/time:** Nov 11th 3pm local (i.e. 3pm on day we are flying)
+**Sim date/time:** Nov 11th 3pm local (i.e. 3pm *on day we are flying*)
 
 **Max start:** 5000 Feet MSL
 
@@ -73,9 +73,9 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         dd.local_time_str = dd.time_el.value;
         dd.local_datetime_str = dd.local_date_str+"T"+dd.local_time_str;
         dd.datetime_js = new Date(dd.local_datetime_str);
-        dd.unix_timestamp_str = (dd.datetime_js.getTime()/1000).toFixed(0);
+        dd.unix_timestamp_int = Math.round(dd.datetime_js.getTime()/1000);
 
-        console.log(`Local datetime: ${dd.local_datetime_str}, timestamp: ${dd.unix_timestamp_str}`);
+        console.log(`Local datetime: ${dd.local_datetime_str}, timestamp: ${dd.unix_timestamp_int}`);
         dd.update_output(dd);
     }
 
@@ -94,6 +94,16 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
 
             // Replace #TIME...#
             display_line = dd.display_replace_time(display_line);
+            output_line = dd.output_replace_time(output_line);
+
+            // Replace **xxxx** with <b>xxxx</b>
+            display_line = dd.display_replace_bold(display_line);
+
+            // Replace *xxxx* with <i>xxxx</i>
+            display_line = dd.display_replace_italic(display_line);
+
+            // Replace https://foo.com with <a href="https://foo.com">https://foo.com</a>
+            display_line = dd.display_replace_url(display_line);
 
             display_str += display_line + "\n";
             dd.output_str += output_line + "\n";
@@ -103,13 +113,53 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
 
     // Replace #DATETIME# with e.g. "Sunday, 11 February 2024 14:00"
     display_replace_datetime(str) {
-        let day_of_week = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")[dd.datetime_js.getDay()];
-        let day_of_month = dd.datetime_js.getDate().toString();
-        let month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December")[dd.datetime_js.getMonth()];
-        let year = dd.datetime_js.getFullYear().toString();
-        let time = ("0"+dd.datetime_js.getHours()).slice(-2) + ":" + ("0"+dd.datetime_js.getMinutes()).slice(-2);
-        let datetime_str = day_of_week+", "+day_of_month+" "+month+" "+year+" "+time;
-        return str.replace("#DATETIME#", datetime_str);
+        let replaced_str = str;
+        let time_pos = replaced_str.indexOf("#DATETIME");
+        while (time_pos >= 0) {
+            let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            let time_str = replaced_str.slice(time_pos, time_end_pos+1);
+            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+
+            // Check for time adjustment
+            let adjust_s = dd.get_adjust_s(time_str);
+
+            let d = new Date(dd.datetime_js); // Avoid side-effect update of dd.datetime_s
+            let adjusted_datetime_js = new Date(d.setSeconds(d.getSeconds() + adjust_s));
+
+            const days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+            let day_of_week = days[adjusted_datetime_js.getDay()];
+            let day_of_month = adjusted_datetime_js.getDate().toString();
+            const months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December");
+            let month = months[adjusted_datetime_js.getMonth()];
+            let year = adjusted_datetime_js.getFullYear().toString();
+            let time = ("0"+adjusted_datetime_js.getHours()).slice(-2) + ":" + ("0"+adjusted_datetime_js.getMinutes()).slice(-2);
+            let datetime_str = day_of_week+", "+day_of_month+" "+month+" "+year+" "+time;
+
+            replaced_str = replaced_str.slice(0,time_pos)+datetime_str+replaced_str.slice(time_end_pos+1);
+
+            time_pos = replaced_str.indexOf("#DATETIME", time_pos);
+        }
+        return replaced_str;
+    }
+
+    output_replace_datetime(str) {
+        let replaced_str = str;
+        let time_pos = replaced_str.indexOf("#DATETIME");
+        while (time_pos >= 0) {
+            let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            let time_str = replaced_str.slice(time_pos, time_end_pos+1);
+            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+
+            // Check for time adjustment
+            let adjust_s = dd.get_adjust_s(time_str);
+
+            let hammertime_str = "<t:"+(dd.unix_timestamp_int + adjust_s).toFixed(0)+":F>";
+
+            replaced_str = replaced_str.slice(0,time_pos)+hammertime_str+replaced_str.slice(time_end_pos+1);
+
+            time_pos = replaced_str.indexOf("#DATETIME", time_pos);
+        }
+        return replaced_str;
     }
 
     // Replace #TIME# with e.g.  "18:45"
@@ -118,30 +168,122 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
     // Replace #TIME-15# with    "18:30"
     display_replace_time(str) {
         let replaced_str = str;
-        let time_pos = str.indexOf("#TIME");
+        let time_pos = replaced_str.indexOf("#TIME");
         while (time_pos >= 0) {
-            let time_end_pos = str.indexOf("#",time_pos+1);
-            let time_str = str.slice(time_pos, time_end_pos+1);
+            let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            let time_str = replaced_str.slice(time_pos, time_end_pos+1);
             console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
 
             // Check for time adjustment
-            let adjust_s = 0;
-            if (time_str.startsWith("#TIME+") || time_str.startsWith("#TIME-")) {
-                // e.g. "#TIME+1:30#"
-                // to "1:30"
-                let adjust_str = time_str.slice(6,-1);
-                // to ["1", "30"] i.e. hours, minutes
-                let adjust_parts = adjust_str.split(":");
+            let adjust_s = dd.get_adjust_s(time_str);
 
-            }
+            let d = new Date(dd.datetime_js); // Avoid side-effect update of dd.datetime_s
+            let adjusted_datetime_js = new Date(d.setSeconds(d.getSeconds() + adjust_s));
 
-            time_pos = str.indexOf("#TIME", time_end_pos);
+            let hh = ("0" + adjusted_datetime_js.getHours()).slice(-2);
+            let mm = ("0" + adjusted_datetime_js.getMinutes()).slice(-2);
+
+            replaced_str = replaced_str.slice(0,time_pos)+hh+":"+mm+replaced_str.slice(time_end_pos+1);
+
+            time_pos = replaced_str.indexOf("#TIME", time_pos);
         }
         return replaced_str;
     }
 
-    output_replace_datetime(str) {
-        return str.replace("#DATETIME#", "<t:"+dd.unix_timestamp_str+":F>");
+    output_replace_time(str) {
+        let replaced_str = str;
+        let time_pos = replaced_str.indexOf("#TIME");
+        while (time_pos >= 0) {
+            let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            let time_str = replaced_str.slice(time_pos, time_end_pos+1);
+            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+
+            // Check for time adjustment
+            let adjust_s = dd.get_adjust_s(time_str);
+
+            let hammertime_str = "<t:"+(dd.unix_timestamp_int + adjust_s).toFixed(0)+":t>";
+            replaced_str = replaced_str.slice(0,time_pos)+hammertime_str+replaced_str.slice(time_end_pos+1);
+
+            time_pos = replaced_str.indexOf("#TIME", time_pos);
+        }
+        return replaced_str;
+    }
+
+    display_replace_bold(str) {
+        let replaced_str = str;
+        let bold_pos = replaced_str.indexOf("**");
+        while (bold_pos >= 0) {
+            let bold_end_pos = replaced_str.indexOf("**",bold_pos+2);
+            let bold_str = replaced_str.slice(bold_pos, bold_end_pos+2);
+            console.log(`${bold_str} between ${bold_pos} and ${bold_end_pos}`);
+
+            replaced_str = replaced_str.slice(0,bold_pos)+"<b>"+bold_str.slice(2,-2)+"</b>"+replaced_str.slice(bold_end_pos+2);
+
+            bold_pos = replaced_str.indexOf("**", bold_pos);
+        }
+        return replaced_str;
+    }
+
+    display_replace_italic(str) {
+        let replaced_str = str;
+        let italic_pos = replaced_str.indexOf("*");
+        while (italic_pos >= 0) {
+            let italic_end_pos = replaced_str.indexOf("*",italic_pos+1);
+            let italic_str = replaced_str.slice(italic_pos, italic_end_pos+1);
+            console.log(`${italic_str} between ${italic_pos} and ${italic_end_pos}`);
+
+            replaced_str = replaced_str.slice(0,italic_pos)+"<i>"+italic_str.slice(1,-1)+"</i>"+replaced_str.slice(italic_end_pos+1);
+
+            italic_pos = replaced_str.indexOf("*", italic_pos);
+        }
+        return replaced_str;
+    }
+
+    display_replace_url(str) {
+        let replaced_str = "";
+        let rx = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;
+        let match;
+        let remaining_str = str;
+        let str_index = 0;
+        while (match = rx.exec(str)) {
+            let url_str = match[0];
+            console.log(`url: [${match.index}..${match.index + match[0].length - 1}] ${url_str}`);
+            replaced_str += str.slice(str_index, match.index);
+            replaced_str += `<a href='${url_str}'>${url_str}</a>`;
+            str_index = match.index + match[0].length;
+        }
+        replaced_str += str.slice(str_index);
+        return replaced_str;
+    }
+
+    get_adjust_s(time_str) {
+        if (time_str.startsWith("#TIME+") || time_str.startsWith("#TIME-")) {
+            let adjust_sign = time_str.slice(5,6) == "+" ? 1 : -1;
+            // e.g. "#TIME+1:30#"
+            // to "1:30"
+            let adjust_str = time_str.slice(6,-1);
+            // to ["1", "30"] i.e. hours, minutes
+            let adjust_parts = adjust_str.split(":");
+            let adjust_s = parseInt(adjust_parts[adjust_parts.length - 1]) * 60; // add minutes
+            if (adjust_parts.length == 2) {
+                adjust_s += parseInt(adjust_parts[0]) * 3600; // add hours
+            }
+            return adjust_sign * adjust_s;
+        } else if (time_str.startsWith("#DATETIME+") || time_str.startsWith("#DATETIME-")) {
+            let adjust_sign = time_str.slice(9,10) == "+" ? 1 : -1;
+            // e.g. "#TIME+1:30#"
+            // to "1:30"
+            let adjust_str = time_str.slice(10,-1);
+            // to ["1", "30"] i.e. hours, minutes
+            let adjust_parts = adjust_str.split(":");
+            let adjust_s = parseInt(adjust_parts[adjust_parts.length - 1]) * 60; // add minutes
+            if (adjust_parts.length == 2) {
+                adjust_s += parseInt(adjust_parts[0]) * 3600; // add hours
+            }
+            return adjust_sign * adjust_s;
+        }
+
+        return 0;
     }
 
     get_time_str(time_str) {
