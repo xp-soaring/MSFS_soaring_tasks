2ec28387f7a36b27b83194f2efa04edb39b98edc
Ian Lewis <ijl20@cam.ac.uk>
2024-02-13 21:19:35 +0000

    discord template WIP

--- AAA_Summary info/discord_template.js
+++ AAA_Summary info/discord_template.js
@@ -3,43 +3,25 @@
 class DiscordDoc {
     constructor() {
         let dd = this;
-        dd.template_str = `
-SSC Task Title
-
-#DATETIME#
-
-**MSFS Server:** Southeast Asia
-
-**Sim date/time:** Nov 11th 3pm local (i.e. 3pm *on day we are flying*)
-
-**Max start:** 5000 Feet MSL
-
-Distance is 300km, expected duration ~90 min
-
-**Meet/Briefing:** #TIME#
-At this time we meet in the voice chat and get ready. https://discord.com/channels/876123356385149009/876397825934626836
-
-**Synchronized Fly:** #TIME+15#
-At this time we simultaneously click the [FLY] button to sync our weather.
-
-**Task Start:** #TIME+45#
-At this time we cross the starting line and start the task.
-
-A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfully during the event will be eligible to apply for the Silver Soaring Badge :silver:
-`;
+        dd.template_str = DiscordDoc.initial_template_str;
     } // end constructor()
 
     init() {
         let dd = this;
 
-        dd.local_date_str = "2024-02-11";
-        dd.local_time_str = "18:45";
+        dd.local_date_str = "2024-02-11"; // Will be derived from date input
+        dd.local_time_str = "18:45"; // Will be derived from time input
 
-        dd.local_datetime_str = dd.local_date_str+"T"+dd.local_time_str;
+        dd.discord_str = "dummy"; // Will contain template updated with Hammertime values
 
-        dd.datetime_js = new Date(dd.local_datetime_str);
+        dd.datetime_js = null; // Date() updated in update_datetime() from current input values
 
-        console.log(dd.datetime_js.toISOString(), Math.round(dd.datetime_js.getTime()/1000));
+        dd.unix_datetime_int = null; // Int unix timestamp updated in update_datetime() from current input values
+
+        dd.MAX_RESET = 5;
+
+        // -------------------------------
+        // Date time input elements
 
         dd.date_el = document.getElementById("input_date");
         dd.date_el.addEventListener('change', function (){
@@ -53,16 +35,34 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
             dd.update_datetime(dd);
         })
 
-
         dd.date_el.valueAsDate = new Date();
-        //dd.time_el.valueAsDate = new Date();
+
+        // -------------------------------
+        // Discord template elements
 
         dd.template_el = document.getElementById("discorddoc_template");
-        dd.template_el.innerText = this.template_str;
+        dd.template_el.innerText = dd.template_str;
+        dd.template_el.addEventListener("input", (event) => {
+            dd.template_updated(dd);
+        });
+
+        document.getElementById("store").addEventListener("click", (e) => {
+            dd.ui_click_store(dd);
+        });
+
+        document.getElementById("reset").addEventListener("click", (e) => {
+            dd.ui_click_reset(dd);
+        });
+
+        // -------------------------------
+        // Display area elements
 
         dd.display_el = document.getElementById("discorddoc_display");
-        dd.output_el = document.getElementById("discorddoc_output");
+        document.getElementById("copy").addEventListener("click", (e) => {
+            dd.ui_click_copy(dd);
+        });
 
+        //----------------------------------------------------------------------
         // trigger an 'update' so the display area is populated
         dd.update_datetime(dd);
     } // end init()
@@ -81,7 +81,7 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
 
     update_output(dd)  {
         let display_str = "";
-        dd.output_str = "";
+        dd.discord_str = "";
 
         let lines = dd.template_str.split("\n");
         for (let i=0; i<lines.length; i++) {
@@ -106,7 +106,7 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
             display_line = dd.display_replace_url(display_line);
 
             display_str += display_line + "\n";
-            dd.output_str += output_line + "\n";
+            dd.discord_str += output_line + "\n";
         }
         dd.display_el.innerHTML = display_str;
     }
@@ -117,8 +117,11 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let time_pos = replaced_str.indexOf("#DATETIME");
         while (time_pos >= 0) {
             let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            if (time_end_pos < 0) {
+                break;
+            }
             let time_str = replaced_str.slice(time_pos, time_end_pos+1);
-            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+            console.log(`Datetime ${time_str} between ${time_pos} and ${time_end_pos}`);
 
             // Check for time adjustment
             let adjust_s = dd.get_adjust_s(time_str);
@@ -147,8 +150,11 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let time_pos = replaced_str.indexOf("#DATETIME");
         while (time_pos >= 0) {
             let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            if (time_end_pos < 0) {
+                break;
+            }
             let time_str = replaced_str.slice(time_pos, time_end_pos+1);
-            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+            console.log(`Datetime ${time_str} between ${time_pos} and ${time_end_pos}`);
 
             // Check for time adjustment
             let adjust_s = dd.get_adjust_s(time_str);
@@ -171,8 +177,11 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let time_pos = replaced_str.indexOf("#TIME");
         while (time_pos >= 0) {
             let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            if (time_end_pos < 0) {
+                break;
+            }
             let time_str = replaced_str.slice(time_pos, time_end_pos+1);
-            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+            console.log(`Time ${time_str} between ${time_pos} and ${time_end_pos}`);
 
             // Check for time adjustment
             let adjust_s = dd.get_adjust_s(time_str);
@@ -195,8 +204,11 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let time_pos = replaced_str.indexOf("#TIME");
         while (time_pos >= 0) {
             let time_end_pos = replaced_str.indexOf("#",time_pos+1);
+            if (time_end_pos < 0) {
+                break;
+            }
             let time_str = replaced_str.slice(time_pos, time_end_pos+1);
-            console.log(`${time_str} between ${time_pos} and ${time_end_pos}`);
+            console.log(`Time ${time_str} between ${time_pos} and ${time_end_pos}`);
 
             // Check for time adjustment
             let adjust_s = dd.get_adjust_s(time_str);
@@ -214,8 +226,11 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let bold_pos = replaced_str.indexOf("**");
         while (bold_pos >= 0) {
             let bold_end_pos = replaced_str.indexOf("**",bold_pos+2);
+            if (bold_end_pos < 0) {
+                break;
+            }
             let bold_str = replaced_str.slice(bold_pos, bold_end_pos+2);
-            console.log(`${bold_str} between ${bold_pos} and ${bold_end_pos}`);
+            console.log(`Bold ${bold_str} between ${bold_pos} and ${bold_end_pos}`);
 
             replaced_str = replaced_str.slice(0,bold_pos)+"<b>"+bold_str.slice(2,-2)+"</b>"+replaced_str.slice(bold_end_pos+2);
 
@@ -229,8 +244,11 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let italic_pos = replaced_str.indexOf("*");
         while (italic_pos >= 0) {
             let italic_end_pos = replaced_str.indexOf("*",italic_pos+1);
+            if (italic_end_pos < 0) {
+                break;
+            }
             let italic_str = replaced_str.slice(italic_pos, italic_end_pos+1);
-            console.log(`${italic_str} between ${italic_pos} and ${italic_end_pos}`);
+            console.log(`Italic ${italic_str} between ${italic_pos} and ${italic_end_pos}`);
 
             replaced_str = replaced_str.slice(0,italic_pos)+"<i>"+italic_str.slice(1,-1)+"</i>"+replaced_str.slice(italic_end_pos+1);
 
@@ -243,13 +261,12 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
         let replaced_str = "";
         let rx = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;
         let match;
-        let remaining_str = str;
         let str_index = 0;
         while (match = rx.exec(str)) {
             let url_str = match[0];
             console.log(`url: [${match.index}..${match.index + match[0].length - 1}] ${url_str}`);
             replaced_str += str.slice(str_index, match.index);
-            replaced_str += `<a href='${url_str}'>${url_str}</a>`;
+            replaced_str += `<a href='${url_str}' target='_blank'>${url_str}</a>`;
             str_index = match.index + match[0].length;
         }
         replaced_str += str.slice(str_index);
@@ -268,17 +285,24 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
             if (adjust_parts.length == 2) {
                 adjust_s += parseInt(adjust_parts[0]) * 3600; // add hours
             }
+            if (isNaN(adjust_s)) {
+                return 0;
+            }
             return adjust_sign * adjust_s;
         } else if (time_str.startsWith("#DATETIME+") || time_str.startsWith("#DATETIME-")) {
             let adjust_sign = time_str.slice(9,10) == "+" ? 1 : -1;
             // e.g. "#TIME+1:30#"
             // to "1:30"
             let adjust_str = time_str.slice(10,-1);
+            console.log(`adj '${adjust_str}'`);
             // to ["1", "30"] i.e. hours, minutes
             let adjust_parts = adjust_str.split(":");
             let adjust_s = parseInt(adjust_parts[adjust_parts.length - 1]) * 60; // add minutes
             if (adjust_parts.length == 2) {
                 adjust_s += parseInt(adjust_parts[0]) * 3600; // add hours
+            }
+            if (isNaN(adjust_s)) {
+                return 0;
             }
             return adjust_sign * adjust_s;
         }
@@ -289,6 +313,70 @@ A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfull
     get_time_str(time_str) {
         return "XXX";
     }
+
+    template_updated(dd) {
+        dd.current_reset = 0;
+        dd.template_str = dd.template_el.innerText;
+        dd.update_output(dd);
+    }
+
+    ui_click_copy(dd) {
+        console.log("copy");
+        navigator.clipboard.writeText(dd.discord_str);
+    }
+
+    ui_click_reset(dd) {
+        console.log("reset");
+        let template_str = localStorage.getItem("template_0");
+        if (template_str != null && template_str != 'null') {
+            dd.template_el.innerText = template_str;
+            for (let i=0; i<dd.MAX_RESET; i++) {
+                template_str = localStorage.getItem("template_"+i);
+                localStorage.setItem("template_"+(i-1), template_str);
+            }
+            localStorage.setItem("template_"+(dd.MAX_RESET-1), 'null');
+        } else {
+            dd.template_el.innerText = DiscordDoc.initial_template_str;
+        }
+        dd.template_updated(dd);
+    }
+
+    ui_click_store(dd) {
+        console.log("store");
+        // Shuffle up existing stored entries
+        for (let i=dd.MAX_RESET - 2; i>=0; i--) {
+            let template_str = localStorage.getItem("template_"+i);
+            if (template_str == null) {
+                continue;
+            }
+            localStorage.setItem("template_"+(i+1), template_str);
+        }
+        localStorage.setItem("template_0", dd.template_str);
+    }
+
+    static initial_template_str = `SSC Task Title
+
+#DATETIME#
+
+**MSFS Server:** Southeast Asia
+
+**Sim date/time:** Nov 11th 3pm local (i.e. 3pm *on day we are flying*)
+
+**Max start:** 5000 Feet MSL
+
+Distance is 300km, expected duration ~90 min
+
+**Meet/Briefing:** #TIME#
+At this time we meet in the voice chat and get ready. https://discord.com/channels/876123356385149009/876397825934626836
+
+**Synchronized Fly:** #TIME+15#
+At this time we simultaneously click the [FLY] button to sync our weather.
+
+**Task Start:** #TIME+45#
+At this time we cross the starting line and start the task.
+
+A scenic trip out of Valparaiso, Chile.  Pilots who finish this task successfully during the event will be eligible to apply for the Silver Soaring Badge :silver:
+`;
 
 } // end class DiscordDoc
 
