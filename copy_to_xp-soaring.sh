e5a178e4f945ba9952fad245efc6e75922ec91bc
Ian Lewis <ijl20@cam.ac.uk>
2024-02-14 16:47:42 +0000

    discord_template WIP

---
+++ copy_to_xp-soaring.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo preparing xp-soaring
+cd ../xp-soaring.github.io
+git pull
+rm -r tasks/discord_template/
+
+echo copying files from MSFS_soaring_tasks
+cd ../MSFS_soaring_tasks
+cp -r discord_template ../xp-soaring.github.io/
+
+cd ../xp-soaring.github.io
+
+echo --------------------------------------------------------
+echo NOW xp-soaring.github.io GIT ADD/COMMIT/PUSH
+echo --------------------------------------------------------
